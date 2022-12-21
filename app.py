42a6467907280566b7f9da30808932dcc0f8d99c
Abderaouf Louggani <ja_louggani@esi.dz>
2022-12-21 10:17:14 +0100

    project init

---
+++ app.py
@@ -0,0 +1,21 @@
+import sqlite3
+from flask import Flask, g
+
+app = Flask(__name__)
+DATABASE = "./db/sqlite.db"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+@app.route("/")
+def index():
+    return "Hello World"
