9c36226d84f398171752c296d72cd03f41e8a48a
oussachi <ousstheouss@outlook.com>
2023-01-18 10:12:59 +0100

    added after_request handling

--- app.py
+++ app.py
@@ -26,6 +26,14 @@ from authInit import oauth
 migrate = Migrate(app, db)
 
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+
 @app.route("/")
 def index():
     return 'Hello, World!'
